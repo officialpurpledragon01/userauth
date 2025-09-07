0f88341ab4a8891422d779030d95011d62e2c68f
Purple Dragon <officialpurpledragon01@mail.com>
2025-09-07 13:39:44 +0100

    dynamic response fixed (color, clear)

--- script.js
+++ script.js
@@ -1,44 +1,48 @@
-let authUser = []
+let authUser = [
+  {mail: 'admin@gmail.com', password: 'admin'}
+]
 
-  //still working on the signin
+// still working on the signin
 function signin() {
   const userMail = document.querySelector('.signinEmail').value;
   const userPassword = document.querySelector('.signinPassword').value;
   let newUser = {mail: userMail, password: userPassword};
 
-	if (userMail === '' || userPassword === '') {
-		document.querySelector('.result2').innerHTML = `all field is required`;
-	} else {
+  if (userMail == '' || userPassword === '') {
+    signinResponse(false);
+    console.error('all field is required');
+    return;
+  } else {
     authUser.push(newUser);
-		document.querySelector('.result2').innerHTML = `<p style="color: green;"> User Created Successfully </p>`;  
-		console.log('success')
-    console.log(authUser) 
-    return clearInput(); 
-	}  
+    signinResponse(true);
+    console.log(`success: new user added`);
+    clearInput(); 
+  }  
 }
 
 function authenticate() {   
   const userMail = document.querySelector('.loginEmail').value;
   const userPassword = document.querySelector('.loginPassword').value;
 
-  
-  for (let i = 0; i < authUser.length; i++) {
-    if (!userMail || !userPassword) {
-        document.querySelector('.result1').innerText = `all field is required`;
-        console.log('no input use this ' + JSON.stringify(authUser[0]))
-    } else {
-      if (userMail === authUser[i].mail && userPassword === authUser[i].password) {
-          document.querySelector('.result1').innerHTML = `<p style="color: green;">User Logged In Successfully</p>`;
-          console.log(`Details Mapped Correct ${JSON.stringify(authUser[i])} = User Input Mail:${userMail}, User Input Password:${userPassword}`);
-      } else {
-          console.log(`this is for demo only (email: ${authUser[i].mail}  ,password: ${authUser[i].password}`);
-          document.querySelector('.result1').innerHTML = `incorrect input check console`;
-      }
-      return clearInput(); 
-    }
+  if (!userMail || !userPassword) {
+    loginResponse(false);
+    console.log('no input! then use this > ' + JSON.stringify(authUser[0]));
+    return;
   }
-  // const test = authUser[i]; //testing
-  // console.log(test.mail) //testing
+
+  let userstatus = false;
+  for (let i = 0; i < authUser.length; i++) {
+    if (userMail === authUser[i].mail && userPassword === authUser[i].password) {
+      console.log(`Details Mapped Correct ${JSON.stringify(authUser[i])} = User Input Mail:${userMail}, User Input Password:${userPassword}`);
+      userstatus = true;
+      loginResponse(userstatus);
+      break;
+    }else {
+    loginResponse('void');
+    console.log(`this is for demo only (email: ${authUser[i].mail}  ,password: ${authUser[i].password})`);
+    }
+  } 
+  clearInput();
 }
 
 function clearInput() {
@@ -46,4 +50,27 @@ function clearInput() {
   document.querySelector('.loginPassword').value = '';
   document.querySelector('.signinEmail').value = '';
   document.querySelector('.signinPassword').value = '';
+}
+function signinResponse(param) {
+  let signin = document.querySelector('.result2');
+  let login = document.querySelector('.result1');
+  if (param == false) {
+    signin.innerHTML = `<p style="color: red;"> all field required </p>`; 
+  } else {
+    signin.innerHTML = `<p style="color: green;"> User Created Successfully </p>`; 
+  }    
+  login.innerHTML = ` `;
+}
+
+function loginResponse(param) {
+  let signin = document.querySelector('.result2');
+  let login = document.querySelector('.result1');
+  if (param == 'void') { 
+  login.innerHTML = `<p style="color: blue">Incorrect input {check console}</p>`;
+  } else if (param == false) {
+    login.innerHTML = `<p style="color: red">all field is required</p>`;
+  } else{
+    login.innerHTML = `<p style="color: green">User logged in successful</p>`;
+  }
+  signin.innerHTML = ` `;
 }
