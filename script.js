a1477665da2c99a4f92604b378268cef66af0729
Purple Dragon <officialpurpledragon01@mail.com>
2025-09-07 11:00:32 +0100

    fixed sign in form

--- script.js
+++ script.js
@@ -1,25 +1,18 @@
-let authUser = {
-      mail: 'admin@gmail.com',
-      password: 'admin'
-  }
+let authUser = []
 
   //still working on the signin
 function signin() {
-	const userMail = document.querySelector('.signinEmail').value;
-	const userPassword = document.querySelector('.signinPassword').value;
-
-  let newUser = {
-		mail: userMail,
-		password: userPassword
-	}
+  const userMail = document.querySelector('.signinEmail').value;
+  const userPassword = document.querySelector('.signinPassword').value;
+  let newUser = {mail: userMail, password: userPassword};
 
 	if (userMail === '' || userPassword === '') {
 		document.querySelector('.result2').innerHTML = `all field is required`;
-
 	} else {
-    authUser = newUser;
+    authUser.push(newUser);
 		document.querySelector('.result2').innerHTML = `<p style="color: green;"> User Created Successfully </p>`;  
-		console.log('success') 
+		console.log('success')
+    console.log(authUser) 
     return clearInput(); 
 	}  
 }
@@ -28,19 +21,26 @@ function authenticate() {
   const userMail = document.querySelector('.loginEmail').value;
   const userPassword = document.querySelector('.loginPassword').value;
 
-  if (!userMail || !userPassword) {
-      document.querySelector('.result1').innerText = `all field is required`;
-  } else {
-    if (userMail === authUser.mail && userPassword === authUser.password) {
-        document.querySelector('.result1').innerHTML = `<p style="color: green;">User Logged In Successfully</p>`;
-        console.log(`Details Mapped Correct ${JSON.stringify(authUser)} = User Input Mail:${userMail}, User Input Password:${userPassword}`);
+  
+  for (let i = 0; i < authUser.length; i++) {
+    if (!userMail || !userPassword) {
+        document.querySelector('.result1').innerText = `all field is required`;
+        console.log('no input use this ' + JSON.stringify(authUser[0]))
     } else {
-        console.log(`this is for demo only (email: ${authUser.mail}  ,password: ${authUser.password}`);
-        document.querySelector('.result1').innerHTML = `incorrect input check console`;   
+      if (userMail === authUser[i].mail && userPassword === authUser[i].password) {
+          document.querySelector('.result1').innerHTML = `<p style="color: green;">User Logged In Successfully</p>`;
+          console.log(`Details Mapped Correct ${JSON.stringify(authUser[i])} = User Input Mail:${userMail}, User Input Password:${userPassword}`);
+      } else {
+          console.log(`this is for demo only (email: ${authUser[i].mail}  ,password: ${authUser[i].password}`);
+          document.querySelector('.result1').innerHTML = `incorrect input check console`;
+      }
+      return clearInput(); 
     }
-    return clearInput(); 
   }
+  // const test = authUser[i]; //testing
+  // console.log(test.mail) //testing
 }
+
 function clearInput() {
   document.querySelector('.loginEmail').value = '';
   document.querySelector('.loginPassword').value = '';
